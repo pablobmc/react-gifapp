cfd1026a876325b584cf13eb5530cab0174f8494
Pablo Morales <pablomc08@gmail.com>
2020-09-14 18:33:48 -0300

    add docs

---
+++ docs/precache-manifest.65c94e12c44b72267e4d09d405e5a36d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5e47d8ec55bfe154646fcc75e2adf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6af02025dcfa44f0da53",
+    "url": "/static/css/main.229783a4.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af02025dcfa44f0da53",
+    "url": "/static/js/main.6b26efc3.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
